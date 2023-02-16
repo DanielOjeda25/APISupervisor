c625357f9b3a6047cc6d7fc2c0828e350624b228
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-02-16 12:19:49 -0300

    rutas eliminadas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from 'express'
 import config from './dbConfig.js'
-import supervisorRoutes from './routes/supervisor.routes.js'
-import localidadesRoutes from './routes/localidad.routes.js'
 import rubrosRouter from './routes/rubros.routes.js'
 import idClientes from './routes/idClientes.routes.js'
 import indexRoute from './routes/index.routes.js'
@@ -10,8 +8,7 @@ const app = express()
 app.set('port', config.port)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(supervisorRoutes)
-app.use(localidadesRoutes)
+
 app.use(rubrosRouter)
 app.use(idClientes)
 app.use(indexRoute)
--- src/routes/localidad.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getLocalidades } from '../controllers/localidad.controller.js'
-
-const router = Router()
-router.get('/localidades', getLocalidades)
-
-export default router
-
