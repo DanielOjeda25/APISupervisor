c066c08a46c66bd400fa1cff3ac868fd3a3973fb
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-02-27 15:31:58 -0300

    corss added

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ import rubrosRouter from './routes/rubros.routes.js'
 import idClientes from './routes/idClientes.routes.js'
 import indexRoute from './routes/index.routes.js'
 import gondolasRoute from './routes/gondolas.routes.js'
+import corss from 'corss'
 
 const app = express()
 app.set('port', config.port)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+app.use(corss())
 app.use(rubrosRouter)
 app.use(idClientes)
 app.use(indexRoute)
