63fd4f079f08180aeb8f667963b12a3f1a686b0c
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2023-03-09 20:42:13 +0000

    "pequeños cambios"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import Home from './components/Home'
-
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import { Switch, Route } from "react-router-dom";
 function App() {
-
-
-  return (
-   <Home />
-  )
+	<div>
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route path="/about" component={About} />
+		</Switch>
+	</div>;
 }
 
-export default App
+export default App;
