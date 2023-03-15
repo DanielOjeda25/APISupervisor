d6b236bd5b37a67f1550b34a243868569727e8da
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-15 16:47:34 -0300

    post gondolas cambiado

--- src/controllers/gondolas.controller.js
+++ src/controllers/gondolas.controller.js
@@ -1,42 +1,34 @@
-import sql from 'mssql/msnodesqlv8.js'
-import dbConfig from '../database/myLocalDB/localConfig.js'
+import sql from "mssql/msnodesqlv8.js";
+import dbConfig from "../database/myLocalDB/localConfig.js";
 
-const queryDB = (
-  rubro,
-  nombre,
-  imagen
-) => {
-  const query = `INSERT INTO gondolasTest ([rubro],[nombre], [imagen]) 
-  VALUES ('${rubro}', '${nombre}', '${imagen}')`
-  return query
-}
+const queryDB = (rubro, nombre, imagen) => {
+  const query = `INSERT INTO [supervisor].[dbo].[gondolasTest] ([rubro],[nombre],[imagen]) 
+  VALUES ('${rubro}', '${nombre}', '${imagen}')`;
+  return query;
+};
 
 export const POSTGondola = async (req, res) => {
   try {
-    const pool = await sql.connect(dbConfig)
-     await pool.request().query(
-      queryDB(
-        req.body['rubro'],
-        req.body['nombre'],
-        req.body['imagen']
-      )
-    )
-    res.status(200).send('Góndola agregada correctamente.')
+    const pool = await sql.connect(dbConfig);
+    await pool
+      .request()
+      .query(
+        queryDB(req.body["rubro"], req.body["nombre"], req.body["imagen"]),
+      );
+    res.status(200).send("Góndola agregada correctamente.");
   } catch (err) {
-    console.log(err)
-    res.status(500).send('Error al agregar la góndola.')
+    console.log(err);
+    res.status(500).send("Error al agregar la góndola.");
   }
-}
+};
 
-const query =
-  'SELECT * FROM gondolasTest'
-
-export const GetGondolas = async (_req, res) => {
+const query = "SELECT  [id] ,[nombre] ,[rubro] ,[imagen] FROM [supervisor].[dbo].[gondolasTest]";
+export const GetGondolas = async (req, res) => {
   try {
-    const pool = await sql.connect(dbConfig)
-    const request = pool.request()
-    const result = await request.query(query)
-    let records = result.recordset
+    const pool = await sql.connect(dbConfig);
+    const request = pool.request();
+    const result = await request.query(query);
+    let records = result.recordset;
     /*  for (let i = 0; i < records.length; i++) { */
     /*    let imagenBuffer = new Buffer.from(records[i].imagen) */
     /*    imagenBuffer = imagenBuffer.toString() */
@@ -44,9 +36,9 @@ export const GetGondolas = async (_req, res) => {
     /*    delete records[i].imagen */
     /*    records[i].imagen = imagenBuffer */
     /*  } */
-    res.status(200).json(records)
+    res.status(200).json(records);
   } catch (err) {
-    console.log(err)
-    res.status(500).send('Error al obtener las góndolas.')
+    console.log(err);
+    res.status(500).send("Error al obtener las góndolas.");
   }
-}
+};
