2dff8dbb8755c8b6bb1b45e23a641eaa2adc57a2
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-15 16:48:11 -0300

    solo se cambio el template

--- src/database/myLocalDB/localDB.js
+++ src/database/myLocalDB/localDB.js
@@ -4,9 +4,9 @@ import dbConfig  from './localConfig.js'
 export default function getConnectionLocal(query) {
   new sql.connect(dbConfig, function (err) {
     if (err) {
-      console.log('Error while connecting database: ' + err)
+      console.log(`Error while connecting database: ${err}`)
     } else {
-      console.log('connected to database: ' + dbConfig.server)
+      console.log(`connected to database: ${dbConfig.server}`)
     }
     const request = new sql.Request()
     request.query(query, function (err, records) {
