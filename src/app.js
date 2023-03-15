be87689d0fc54c208d1eb7abe6207d98f69a614b
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-15 11:51:43 -0300

    se agrego el bodyParser

--- src/app.js
+++ src/app.js
@@ -5,11 +5,12 @@ import idClientes from './routes/idClientes.routes.js'
 import indexRoute from './routes/index.routes.js'
 import gondolasRoute from './routes/gondolas.routes.js'
 import cors from 'cors'
-
+import bodyParser from 'body-parser'
 const app = express()
 app.set('port', config.port)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use(rubrosRouter)
 app.use(idClientes)
