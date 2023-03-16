56d234a0eeb62b071fcfc0f1b85c152e620ffaea
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-16 14:46:05 -0300

    se agrego un loading

--- client/src/components/Gondolas.jsx
+++ client/src/components/Gondolas.jsx
@@ -3,12 +3,14 @@ import { useForm } from "react-hook-form";
 import { optionsRubros } from "../utils/gondolasUtils";
 import BackButton from "./BotomBack";
 import axios from "axios";
+import Spinner from "./spinner";
 function Gondolas() {
 	const [data, setData] = useState([]);
 	const [rubrosSeleccionado, setRubrosSeleccionado] = useState("");
 	const [clientesSeleccionado, setClientesSeleccionado] = useState("");
 	const [imagenSeleccionada, setImagenSeleccionada] = useState(null);
-	const [opcionSeleccionada, setOpcionSeleccionada] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+
 	const {
 		register,
 		handleSubmit,
@@ -17,8 +19,16 @@ function Gondolas() {
 
 	useEffect(() => {
 		const fetchData = async () => {
-			const response = await axios("http://10.211.55.5:8080/idClientes");
-			setData(response.data);
+			setIsLoading(true);
+			try {
+				const response = await axios("http://10.211.55.5:8080/idClientes");
+				setData(response.data);
+			} catch (error) {
+				console.error(error);
+				alert("Ha ocurrido un error al cargar los clientes.");
+			} finally {
+				setIsLoading(false);
+			}
 		};
 
 		fetchData();
@@ -51,10 +61,9 @@ function Gondolas() {
 			console.error(error);
 			alert("Ha ocurrido un error al enviar el formulario.");
 		} finally {
+			setImagenSeleccionada(null);
 			setClientesSeleccionado("");
 			setRubrosSeleccionado("");
-			setImagenSeleccionada(null);
-			setOpcionSeleccionada("");
 		}
 	};
 
@@ -67,107 +76,104 @@ function Gondolas() {
 
 	const handleClientesChange = (event) => {
 		setClientesSeleccionado(event.target.value);
-		setOpcionSeleccionada(event.target.value);
 	};
 	const handleImagenChange = (event) => {
 		setImagenSeleccionada(event.target.files[0]);
 	};
 	return (
 		<div className="flex justify-center items-center h-screen bg-gray-800 flex-col">
-			<BackButton />
-
-			<form
-				onSubmit={handleSubmit(onSubmit)}
-				className="max-w-md mx-auto border border-gray-200 p-6 rounded-lg bg-slate-100"
-			>
-				<div className="mb-4">
-					<label htmlFor="rubros" className="block mb-2 font-bold">
-						Rubros:
-					</label>
-					<select
-						id="rubros"
-						{...register("rubros", { required: true })}
-						value={rubrosSeleccionado}
-						onChange={handleRubrosChange}
-						className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500"
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<>
+					<BackButton />
+					<form
+						onSubmit={handleSubmit(onSubmit)}
+						className="max-w-md mx-auto border border-gray-200 p-6 rounded-lg bg-slate-100"
 					>
-						<option value="">Seleccione un rubro</option>
-						{optionsRubros.map((option) => (
-							<option key={option.value} value={option.value}>
-								{option.value}
-							</option>
-						))}
-					</select>
-					{errors.rubros && (
-						<span className="text-red-500">Campo requerido</span>
-					)}
-				</div>
-				<div className="mb-4">
-					<label htmlFor="clientes" className="block mb-2 font-bold">
-						Clientes:
-					</label>
-					<select
-						id="clientes"
-						{...register("clientes", { required: true })}
-						value={clientesSeleccionado}
-						onChange={handleClientesChange}
-						className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500"
-					>
-						<option value="">Seleccione un cliente</option>
-						{data.map((option) => (
-							<option key={generateKey()} value={option.value}>
-								{option.nombre}
-							</option>
-						))}
-					</select>
-					{errors.clientes && (
-						<span className="text-red-500">Campo requerido</span>
-					)}
-				</div>
-				<div className="mb-4">
-					<label htmlFor="imagen" className="block mb-2 font-bold">
-						Imagen:
-					</label>
-					<div className="relative">
-						<input
-							id="imagen"
-							type="file"
-							{...register("imagen", { required: true })}
-							onChange={handleImagenChange}
-							className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500 bg-white"
-						/>
-						<div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-							<svg
-								className="h-6 w-6 text-gray-400"
-								fill="none"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
+						<div className="mb-4">
+							<label htmlFor="rubros" className="block mb-2 font-bold">
+								Rubros:
+							</label>
+							<select
+								id="rubros"
+								{...register("rubros", { required: true })}
+								value={rubrosSeleccionado}
+								onChange={handleRubrosChange}
+								className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500"
 							>
-								<path d="M12 4v16m8-8H4" />
-							</svg>
+								<option value="">Seleccione un rubro</option>
+								{optionsRubros.map((option) => (
+									<option key={option.value} value={option.value}>
+										{option.value}
+									</option>
+								))}
+							</select>
+							{errors.rubros && (
+								<span className="text-red-500">Campo requerido</span>
+							)}
 						</div>
-					</div>
-					{errors.imagen && (
-						<span className="text-red-500">Campo requerido</span>
-					)}
-					{imagenSeleccionada && (
-						<img
-							src={URL.createObjectURL(imagenSeleccionada)}
-							alt="Imagen seleccionada"
-						/>
-					)}
-				</div>
+						<div className="mb-4">
+							<label htmlFor="clientes" className="block mb-2 font-bold">
+								Clientes:
+							</label>
+							<select
+								id="clientes"
+								{...register("clientes", { required: true })}
+								value={clientesSeleccionado}
+								onChange={handleClientesChange}
+								className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500"
+							>
+								<option value="">Seleccione un cliente</option>
+								{data.map((option) => (
+									<option key={generateKey()} value={option.value}>
+										{option.nombre}
+									</option>
+								))}
+							</select>
+							{errors.clientes && (
+								<span className="text-red-500">Campo requerido</span>
+							)}
+						</div>
+						<div className="mb-4">
+							<label className="block mb-2 font-bold" htmlFor="imagen">
+								Imagen:
+							</label>
+							<input
+								{...register("imagen", { required: true })}
+								onChange={handleImagenChange}
+								className="block w-full mb-5 text-xs text-gray-900  rounded cursor-pointer bg-gray-50 focus:outline-none"
+								id="imagen"
+								type="file"
+							/>
+							{errors.imagen && (
+								<span className="text-red-500">Campo requerido</span>
+							)}
+							{imagenSeleccionada && (
+								<img
+									src={URL.createObjectURL(imagenSeleccionada)}
+									alt="Imagen seleccionada"
+									className="w-full h-auto"
+								/>
+							)}
+						</div>
 
-				<button
-					type="submit"
-					className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
-				>
-					Enviar
-				</button>
-			</form>
+						<button
+							type="submit"
+							className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
+						>
+							<svg
+								className="w-4 h-4 mr-2 text-white inline-block align-text-bottom"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+							>
+								<path fill="currentColor" d="M10 0l8 8h-6v12h-4v-12h-6l8-8z" />
+							</svg>
+							Enviar
+						</button>
+					</form>
+				</>
+			)}
 		</div>
 	);
 }
