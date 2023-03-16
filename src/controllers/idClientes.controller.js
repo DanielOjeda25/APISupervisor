4b6eebd225c8352e2c80c95324fede1ea39d99fc
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-16 11:37:44 -0300

    Metodo optimizado

--- src/controllers/idClientes.controller.js
+++ src/controllers/idClientes.controller.js
@@ -1,29 +1,15 @@
-import sql from 'mssql/msnodesqlv8.js'
-import dbConfig from '../dbConfig.js'
-
-const query =
-  "SELECT DISTINCT [idcliente],[nombre],[nombre_localidad],[vendedor],[supervisor] FROM [Bamana].[dbo].[vwCuboClientes] WHERE [nombre_provincia] = 'MISIONES' ORDER BY [nombre] ASC"
-//
-//old query SELECT DISTINCT [idcliente],[nombre],[nombre_localidad],[nombre_provincia],[idvendedor],[vendedor],[supervisor] FROM [Bamana].[dbo].[vwCuboClientes] WHERE [nombre_provincia] = 'MISIONES'
+import pool from './pool.js'
 
 export const getIdCliente = async (req, res) => {
-  const conexion = (err) => {
-    if (err) {
-      console.log(`Error while connecting database: ${err}`)
-    } else {
-      console.log(`connected to database: ${dbConfig.server}`)
-    }
-    const request = new sql.Request()
+  const query =
+    "SELECT DISTINCT [idcliente],[nombre],[nombre_localidad],[vendedor],[supervisor] FROM [Bamana].[dbo].[vwCuboClientes] WHERE [nombre_provincia] = 'MISIONES' ORDER BY [nombre] ASC"
 
-    request.query(query, (err, records) => {
-      if (err) console.log(err)
-      res.set('Content-Type', 'application/json')
-      res.json(records.recordset)
-    })
-  }
   try {
-    new sql.connect(dbConfig, conexion)
+    const request = pool.request()
+    const result = await request.query(query)
+    res.json(result.recordset)
   } catch (err) {
-    console.log(err)
+    console.log(`Error while connecting database: ${err}`)
+    res.status(500).send('Internal server error')
   }
 }
