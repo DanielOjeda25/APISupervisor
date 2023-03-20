c186b511fdfe2958a570d061ac70ad76a70d112f
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-20 16:23:13 -0300

    cambios minimos, falta

--- src/controllers/gondolas.controller.js
+++ src/controllers/gondolas.controller.js
@@ -1,42 +1,47 @@
 import sql from "mssql/msnodesqlv8.js";
-import dbConfig from "../database/myLocalDB/localConfig.js"
+import dbConfig from "../database/myLocalDB/localConfig.js";
 
 const queryDB = (rubro, nombre, imagen) => {
-  return `INSERT INTO supervisor.dbo.gondolasTest ([rubro],[nombre],[imagen]) 
+	return `INSERT INTO supervisor.dbo.gondolasTest ([rubro],[nombre],[imagen]) 
     VALUES ('${rubro}', '${nombre}', '${imagen}')`;
 };
 
 export const POSTGondola = async (req, res) => {
-  try {
-    const pool = await sql.connect(dbConfig);
-    const query = queryDB(req.body["rubro"], req.body["nombre"], req.body["imagen"]);
-    await pool.request().query(query);
-    res.status(200).send("Góndola agregada correctamente.");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Error al agregar la góndola.");
-  }
+	try {
+		const pool = await sql.connect(dbConfig);
+		const query = queryDB(
+			req.body["rubro"],
+			req.body["nombre"],
+			req.body["imagen"],
+		);
+		await pool.request().query(query);
+		res.status(200).send("Góndola agregada correctamente.");
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("Error al agregar la góndola.");
+	}
 };
 const queryDBGondolas = () => {
-  return "SELECT [id], [nombre], [rubro], [imagen] FROM [supervisor].[dbo].[gondolasTest]";
+	return "SELECT [id], [nombre], [rubro], [imagen] FROM [supervisor].[dbo].[gondolasTest]";
 };
 
 export const GetGondolas = async (req, res) => {
-  try {
-    const pool = await sql.connect(dbConfig);
-    const query = queryDBGondolas();
-    const result = await pool.request().query(query);
-    let records = result.recordset;
-    /*  for (let i = 0; i < records.length; i++) { */
-    /*    let imagenBuffer = new Buffer.from(records[i].imagen) */
-    /*    imagenBuffer = imagenBuffer.toString() */
-    /*    // rome-ignore lint/performance/noDelete: <explanation> */
-    /*    delete records[i].imagen */
-    /*    records[i].imagen = imagenBuffer */
-    /*  } */
-    res.status(200).json(records);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Error al obtener las góndolas.");
-  }
-}
+	try {
+		const pool = await sql.connect(dbConfig);
+		const query = queryDBGondolas();
+		const result = await pool.request().query(query);
+		let records = result.recordset;
+		for (let i = 0; i < records.length; i++) {
+			let imagenBuffer = new Buffer.from(records[i].imagen);
+			imagenBuffer = imagenBuffer.toString();
+      console.log(imagenBuffer)
+			// rome-ignore lint/performance/noDelete: <explanation> */
+			delete records[i].imagen;
+			records[i].imagen = imagenBuffer;
+		}
+		res.status(200).json(records);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("Error al obtener las góndolas.");
+	}
+};
