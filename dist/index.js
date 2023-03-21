86c494f1f5187d1cc91f4c5ea100a886a8595d01
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-21 11:06:08 -0300

    archivos preparados para produccion

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _app = _interopRequireDefault(require("./app.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_app["default"].listen(8080);
+console.log("server on port http://localhost:8080/");
