2112c03c8a5cbac708a8cb75bc4f4f9fbb668048
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-03-23 12:54:34 -0300

    pequeños cambios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import router from "./App";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<RouterProvider router={router}/>
+			<RouterProvider router={router} />
 	</React.StrictMode>,
 );
